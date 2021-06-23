9cb67b8b35cdb82aeecaf8fe5994110c1ab9d736
Raloseq <rafcio227@op.pl>
2021-06-23 22:04:18 +0200

    add controller for handling actions

--- index.php
+++ index.php
@@ -4,43 +4,11 @@ declare(strict_types=1);
 
 namespace App;
 
-require_once("src/Utils/debug.php");
-require_once("src/View.php");
+require_once "src/Utils/debug.php";
+require_once "src/Controller.php";
 
 //error_reporting(0);
 //ini_set('display_errors', '0');
 
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$view = new View();
-
-$viewParams = [];
-
-switch ($action) {
-    case 'create':
-        $page = 'create';
-        $created = false;
-        if(!empty($_POST)) {
-            $created = true;
-            $viewParams = [
-                'title' => $_POST['title'],
-                'description' => $_POST['description']
-            ];
-        }
-
-        $viewParams['created'] = $created;
-        break;
-    case 'show':
-        $viewParams = [
-            'title' => $_POST['title'],
-            'description' => $_POST['description']
-        ];
-        break;
-    default:
-        $page = 'list';
-        break;
-}
-
-$view->render($page, $viewParams);
+$controller = new Controller($_GET,$_POST);
+$controller->run();
