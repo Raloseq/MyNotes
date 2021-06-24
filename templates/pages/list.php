d84fc2e424eede23e07c1b3564fb4dc97c316e22
Raloseq <rafcio227@op.pl>
2021-06-24 20:43:11 +0200

    created layout where we are showing notes

--- templates/pages/list.php
+++ templates/pages/list.php
@@ -1,14 +1,34 @@
-<section>
-    <div class="message">
-        <?php
-            if(!empty($params['before'])) {
-                switch ($params['before']) {
-                    case 'created':
-                        echo "Note created";
-                        break;
+<div class="list">
+    <section>
+        <div class="message">
+            <?php
+                if(!empty($params['before'])) {
+                    switch ($params['before']) {
+                        case 'created':
+                            echo "Note created";
+                            break;
+                    }
                 }
-            }
-        ?>
-    </div>
-    <h2>List of notes</h2>
-</section>
+            ?>
+        </div>
+
+        <div class="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </section>
+</div>
