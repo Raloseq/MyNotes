91d3d7ce3534723bc52c66ad4e97258d34789a0b
Raloseq <rafcio227@op.pl>
2021-07-01 22:11:03 +0200

    added config file

--- config/config.php
+++ config/config.php
@@ -1,1 +1,9 @@
 <?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'database' => 'mynotes',
+        'user' => 'user_mynotes',
+        'password' => 'b5345a9BNsbRExss'
+    ]
+];
